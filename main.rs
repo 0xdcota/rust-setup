01ca18007075fddc9e6015410606bf5a66813094
0xdcota <32775237+0xdcota@users.noreply.github.com>
2026-02-24 23:13:10 +0000

    chore: assignment 2 create function in main.rs and describe in README.md

---
+++ main.rs
@@ -0,0 +1,7 @@
+/// Value of Pi with 18 decimals as constant
+const PI: f64 = 3.141592653589793238;
+
+// Function to calculate the area of a circle given its radius
+fn area_of_circle(radius: f64) -> f64 {
+    return PI * radius * radius;
+}
